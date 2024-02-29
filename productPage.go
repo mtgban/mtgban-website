768f94e268b045c2431c7e308bd4738a8c8cf9a1
the-muppet <jette@mtgban.com>
2024-02-29 00:29:30 -0600

    added page to house subscription pricing

---
+++ productPage.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"net/http"
+	"strings"
+	"time"
+)
+
+// Handler for / renders the home.html page
+func ProductPage(w http.ResponseWriter, r *http.Request) {
+	sig := getSignatureFromCookies(r)
+	errmsg := r.FormValue("errmsg")
+	message := ""
+
+	switch errmsg {
+	case "TokenNotFound":
+		message = "There was a problem authenticating you with Patreon."
+	case "UserNotFound", "TierNotFound":
+		message = ErrMsg
+	case "logout":
+		domain := "mtgban.com"
+		if strings.Contains(getBaseURL(r), "localhost") {
+			domain = "localhost"
+		}
+
+		// Invalidate the current cookie
+		cookie := http.Cookie{
+			Name:    "MTGBAN",
+			Domain:  domain,
+			Path:    "/",
+			Expires: time.Now(),
+		}
+		http.SetCookie(w, &cookie)
+
+		// Delete signature
+		sig = ""
+	}
+
+	pageVars := genPageNav("ProductPage", sig)
+	pageVars.ErrorMessage = message
+
+	render(w, "home.html", pageVars)
+}
