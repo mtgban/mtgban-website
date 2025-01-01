feec07e007a07e88e342edf9b0800d3550a497a8
Vittorio Giovara <vittorio.giovara@gmail.com>
2025-01-01 02:26:51 -0500

    Change the go-mtgban script to use the latest tag

--- update-mtgban.sh
+++ update-mtgban.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-TIP=$(git ls-remote https://github.com/mtgban/go-mtgban.git HEAD | awk '{ print $1}')
+# from https://stackoverflow.com/a/12704727/377439
+TIP=$(git -c 'versionsort.suffix=-' ls-remote --exit-code --refs \
+      --sort='version:refname' --tags  https://github.com/mtgban/go-mtgban.git '*.*.*' | \
+      tail --lines=1 | cut -d "/" -f 3)
 
-GOSUMDB=off go get -u github.com/mtgban/go-mtgban@$TIP
+go get -u github.com/mtgban/go-mtgban@$TIP
 
 go mod tidy
