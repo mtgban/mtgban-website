e087659ed15b9b96ca33b0e570ad8ac23a885f78
Vittorio Giovara <vittorio.giovara@gmail.com>
2025-05-23 01:04:18 -0400

    search: Show the price equivalence if paying with store credit

--- search.go
+++ search.go
@@ -813,6 +813,7 @@ func searchSellersNG(cardIds []string, config SearchConfig) (foundSellers map[st
 					ScraperName: name,
 					Shorthand:   seller.Info().Shorthand,
 					Price:       entry.Price,
+					Credit:      entry.Price / seller.Info().CreditMultiplier,
 					Quantity:    entry.Quantity,
 					URL:         entry.URL,
 					NoQuantity:  seller.Info().NoQuantityInventory || seller.Info().MetadataOnly,
