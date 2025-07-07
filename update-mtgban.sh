b157101df2a62a416e8d5fbaad3e2f45eb462d80
Vittorio Giovara <vittorio.giovara@gmail.com>
2025-07-07 02:52:23 +0200

    Allow to update go-mtgban at the latest commit
    Avoids committing and tagging too many releases for experiments.

--- update-mtgban.sh
+++ update-mtgban.sh
@@ -5,6 +5,12 @@ TIP=$(git -c 'versionsort.suffix=-' ls-remote --exit-code --refs \
       --sort='version:refname' --tags  https://github.com/mtgban/go-mtgban.git '*.*.*' | \
       tail --lines=1 | cut -d "/" -f 3)
 
+if [[ "${1}" == "latest" ]]
+then
+    echo "Using latest hash"
+    TIP=$(git ls-remote https://github.com/mtgban/go-mtgban.git HEAD | awk '{ print $1}')
+fi
+
 go get -u github.com/mtgban/go-mtgban@$TIP
 
 go mod tidy
