f24404ca6b93f18d4e03d129c0febc958918df47
Vittorio Giovara <vittorio.giovara@gmail.com>
2025-07-17 20:16:29 +0200

    search: Display the full name for DFCSameName cards

--- js/autocomplete.js
+++ js/autocomplete.js
@@ -36,14 +36,6 @@ async function autocomplete(form, inp, sealed) {
 
         /* For each item in the array... */
         for (i = 0; i < arr.length; i++) {
-            /* Skip DFC with same name */
-            if (arr[i].includes(" // ")) {
-                const subarr = arr[i].split(" // ");
-                if (subarr[0] == subarr[1]) {
-                    continue;
-                }
-            }
-
             /* Check if the item starts with the same letters as the text field value */
             if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase() ||
                 arr[i].substr(0, val.length).normalize("NFD").replace(/[\u0300-\u036f]/g, "").toUpperCase() == val.toUpperCase() ||
