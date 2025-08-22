fd7a3bd1b9f0ad1291eb734a84dbb17c042821d0
Vittorio Giovara <vittorio.giovara@gmail.com>
2025-08-22 22:20:29 +0200

    Move data updating functions to the appropriate file
    Make array looping uniform between seller and vendor (no functional
    changes).

--- load.go
+++ load.go
@@ -3,6 +3,7 @@ package main
 import (
 	"compress/gzip"
 	"context"
+	"errors"
 	"fmt"
 	"io"
 	"log"
@@ -159,4 +160,116 @@ func loadData(pathOpt string) (io.ReadCloser, error) {
 	}
 
 	return reader, err
+}
+
+func updateSellers(scraper mtgban.Scraper) {
+	sellerIndex := -1
+	for i, seller := range Sellers {
+		if seller != nil && seller.Info().Shorthand == scraper.Info().Shorthand {
+			sellerIndex = i
+			break
+		}
+	}
+
+	err := updateSellerAtPosition(scraper.(mtgban.Seller), sellerIndex)
+	if err != nil {
+		msg := fmt.Sprintf("seller %s %s - %s", scraper.Info().Name, scraper.Info().Shorthand, err.Error())
+		ServerNotify("refresh", msg, true)
+		return
+	}
+
+	msg := fmt.Sprintf("%s inventory updated at position %d", scraper.Info().Shorthand, sellerIndex)
+	ServerNotify("refresh", msg)
+
+	return
+}
+
+func updateSellerAtPosition(seller mtgban.Seller, i int) error {
+	// Save seller in global array
+	if i < 0 {
+		Sellers = append(Sellers, seller)
+		return nil
+	}
+
+	// Load inventory
+	inv, err := seller.Inventory()
+	if err != nil {
+		return err
+	}
+
+	// Do not update in case the new inventory wasn't completely loaded
+	// for example due to API problems
+	old, _ := Sellers[i].Inventory()
+	if len(inv) > 0 && len(inv) < len(old)/2 {
+		return errors.New("new inventory is missing too many entries")
+	}
+
+	// Make sure the input seller is _only_ a Seller and not anything
+	// else, so that filtering works like expected
+	outSeller := mtgban.NewSellerFromInventory(inv, seller.Info())
+
+	// Save seller in global array
+	if i < 0 {
+		Sellers = append(Sellers, outSeller)
+	} else {
+		Sellers[i] = outSeller
+	}
+
+	return nil
+}
+
+func updateVendors(scraper mtgban.Scraper) {
+	vendorIndex := -1
+	for i, vendor := range Vendors {
+		if vendor != nil && vendor.Info().Shorthand == scraper.Info().Shorthand {
+			vendorIndex = i
+			break
+		}
+	}
+
+	err := updateVendorAtPosition(scraper.(mtgban.Vendor), vendorIndex)
+	if err != nil {
+		msg := fmt.Sprintf("vendor %s %s - %s", scraper.Info().Name, scraper.Info().Shorthand, err.Error())
+		ServerNotify("refresh", msg, true)
+		return
+	}
+
+	msg := fmt.Sprintf("%s buylist updated at position %d", scraper.Info().Shorthand, vendorIndex)
+	ServerNotify("refresh", msg)
+
+	return
+}
+
+func updateVendorAtPosition(vendor mtgban.Vendor, i int) error {
+	// Save vendor in global array
+	if i < 0 {
+		Vendors = append(Vendors, vendor)
+		return nil
+	}
+
+	// Load buylist
+	bl, err := vendor.Buylist()
+	if err != nil {
+		return err
+	}
+
+	// Do not update in case the new buylist wasn't completely loaded
+	// for example due to API problems
+	old, _ := Vendors[i].Buylist()
+	if len(bl) > 0 && len(bl) < len(old)/2 {
+		return errors.New("new buylist is missing too many entries")
+	}
+
+	// Save vendor in global array, making sure it's _only_ a Vendor
+	// and not anything esle, so that filtering works like expected
+	outVendor := mtgban.NewVendorFromBuylist(bl, vendor.Info())
+
+	// Save vendor in global array
+	if i < 0 {
+		Vendors = append(Vendors, outVendor)
+	} else {
+		Vendors[i] = outVendor
+	}
+
+	return nil
 }
