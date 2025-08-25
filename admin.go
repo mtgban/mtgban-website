mtgban/mtgban-website

commit 1162462b500833f98a4ca2acd673be2f26d005a9

admin: Protect the first run in case Config is not loaded

admin.go
@@ -739,6 +739,9 @@ func generateAPIKey(link, user string, duration time.Duration) (string, error) {
 	if !found {
 		key = randomString(15)
 		apiUsersMutex.Lock()
+		if Config.ApiUserSecrets == nil {
+			return "", errors.New("config not loaded")
+		}
 		Config.ApiUserSecrets[user] = key
 		apiUsersMutex.Unlock()
 

commit 97e8a728f47f6a55790b881ac532611c070adc58

admin: Remove demo key generation from a hot path
Also incorrect mutex exit introduced in
1162462b500833f98a4ca2acd673be2f26d005a9.

admin.go
@@ -346,6 +346,17 @@ func Admin(w http.ResponseWriter, r *http.Request) {
 			os.Exit(0)
 		}()
 
+	case "demokey":
+		key, err := generateAPIKey(getBaseURL(r), DefaultAPIDemoUser, DefaultAPIDemoKeyDuration)
+		if err != nil {
+			log.Println(err)
+			pageVars.InfoMessage = err.Error()
+			break
+		}
+		pageVars.DemoKey = url.QueryEscape(key)
+
+		log.Println(pageVars.DemoKey)
+
 	case "newKey":
 		v = url.Values{}
 		doReboot = true
@@ -633,7 +644,6 @@ func Admin(w http.ResponseWriter, r *http.Request) {
 	pageVars.MemoryStatus = mem()
 	pageVars.LatestHash = BuildCommit
 	pageVars.CurrentTime = time.Now()
-	pageVars.DemoKey = url.QueryEscape(getDemoKey(getBaseURL(r)))
 
 	render(w, "admin.html", pageVars)
 }
@@ -895,11 +905,6 @@ const (
 	DefaultAPIDemoUser        = "demo@mtgban.com"
 )
 
-func getDemoKey(link string) string {
-	key, _ := generateAPIKey(link, DefaultAPIDemoUser, DefaultAPIDemoKeyDuration)
-	return key
-}
-
 var apiUsersMutex sync.RWMutex
 
 func writeConfigFile(config ConfigType, writer io.Writer) error {
@@ -921,10 +926,12 @@ func generateAPIKey(link, user string, duration time.Duration) (string, error) {
 
 	if !found {
 		key = randomString(15)
-		apiUsersMutex.Lock()
+
 		if Config.ApiUserSecrets == nil {
 			return "", errors.New("config not loaded")
 		}
+
+		apiUsersMutex.Lock()
 		Config.ApiUserSecrets[user] = key
 		apiUsersMutex.Unlock()
 

templates/admin.html
@@ -170,12 +170,6 @@
                     </form>
                 </li>
                 <li>
-                    API Demo Key
-                    <form>
-                        <input value="{{.DemoKey}}" onClick="this.setSelectionRange(0, this.value.length)" class="input-css">
-                    </form>
-                </li>
-                <li>
                     Generate New Key
                     <form>
                         <input type="hidden" name="reboot" value="newKey"/>
@@ -188,6 +182,17 @@
                                 <option value="183">Half year</option>
                                 <option value="365">One year</option>
                         </select>
+                    </form>
+                </li>
+                <li>
+                    API Demo Key
+                    <form {{if not .DemoKey}}action="admin" method="GET"{{end}}>
+                        {{if .DemoKey}}
+                            <input value="{{.DemoKey}}" onClick="this.setSelectionRange(0, this.value.length)" class="input-css">
+                        {{else}}
+                            <input type="hidden" name="reboot" value="demokey"/>
+                            <input type="submit" value="&nbsp;&nbsp;Click to get a Demo API Key&nbsp;&nbsp;"/>
+                        {{end}}
                     </form>
                 </li>
             </ul>