31a59684a56a345bd11e027bbb8c346fec94c4e7
Vittorio Giovara <vittorio.giovara@gmail.com>
2025-09-21 11:59:10 +0200

    arbit: Fix condition option being reset

--- arbit.go
+++ arbit.go
@@ -762,7 +762,6 @@ func scraperCompare(w http.ResponseWriter, r *http.Request, pageVars PageVars, a
 		}
 
 		// Set custom scraper options
-		opts.Conditions = nil
 		if pageVars.GlobalMode && scraper.Info().Shorthand == "TCGDirect" {
 			opts.Conditions = BadConditions
 		}
