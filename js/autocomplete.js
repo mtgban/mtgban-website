ac88f906df0e8bcb52856f09b40b812c0f74f368
Vittorio Giovara <vittorio.giovara@gmail.com>
2025-09-25 17:21:45 +0200

    js/autocomplete: Move input text to its own variable

--- js/autocomplete.js
+++ js/autocomplete.js
@@ -36,10 +36,11 @@ async function autocomplete(form, inp, sealed) {
 
         /* For each item in the array... */
         for (i = 0; i < arr.length; i++) {
+            let inputText = val.toUpperCase();
             /* Check if the item starts with the same letters as the text field value */
-            if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase() ||
-                arr[i].substr(0, val.length).normalize("NFD").replace(/[\u0300-\u036f]/g, "").toUpperCase() == val.toUpperCase() ||
-                arr[i].replace(/[^A-Za-z0-9 ]/g, "").substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+            if (arr[i].substr(0, val.length).toUpperCase() == inputText ||
+                arr[i].substr(0, val.length).normalize("NFD").replace(/[\u0300-\u036f]/g, "").toUpperCase() == inputText ||
+                arr[i].replace(/[^A-Za-z0-9 ]/g, "").substr(0, val.length).toUpperCase() == inputText) {
                 /* Create a DIV element for each matching element */
                 b = document.createElement("DIV");
 
