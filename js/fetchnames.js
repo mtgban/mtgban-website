02d8c09efc20ea5dca46d5234516b228c5b0db6a
Vittorio Giovara <vittorio.giovara@gmail.com>
2025-12-30 01:03:59 -0500

    js/fetchnames: Simplify URL and drop outdated comment

--- js/fetchnames.js
+++ js/fetchnames.js
@@ -1,10 +1,5 @@
-const url = "/api/suggest?all=true";
-
-/*
- * Query Scryfall to retrieve the list of card names.
- */
 async function fetchNames(sealed) {
-    let cardNames = await fetch(url + "&sealed=" + sealed)
+    let cardNames = await fetch("/api/suggest?all=true&sealed=" + sealed)
         // Transform the data into json
         .then(response => response.json())
     return cardNames;
