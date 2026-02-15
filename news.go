cd832a21bbd16a054e0484e9e5460d2c1bed78a4
Vittorio Giovara <vittorio.giovara@gmail.com>
2026-02-15 22:06:53 +0100

    newspaper: Implement filtering on the New Newspaper

--- news.go
+++ news.go
@@ -1556,6 +1556,35 @@ func Newspaper(w http.ResponseWriter, r *http.Request) {
 		}
 		pageVars.Table = output
 	} else {
+		if skipEditionsOpt != "" || rarity != "" || filter != "" {
+			var output [][]string
+			for _, result := range results {
+				cardRarity := result[0]
+				edition := result[4]
+				if skipEditionsOpt != "" {
+					filters := strings.Split(skipEditionsOpt, ",")
+					if slices.Contains(filters, edition) {
+						continue
+					}
+				}
+				if filter != "" && filter != edition {
+					continue
+				}
+				if rarity != "" && cardRarity != "" {
+					if strings.Contains(rarity, "/") {
+						rarities := strings.Split(rarity, "/")
+						if string(cardRarity[0]) != strings.ToLower(rarities[0]) && string(cardRarity[0]) != strings.ToLower(rarities[1]) {
+							continue
+						}
+					} else if string(cardRarity[0]) != strings.ToLower(rarity) {
+						continue
+					}
+				}
+				output = append(output, result)
+			}
+
+			results = output
+		}
 		pageVars.Table, pageVars.Pagination = Paginate(results, pageIndex, 50, len(results))
 	}
 
