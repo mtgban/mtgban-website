468eba5965aca12550ac89765c5221ae375bf1ea
Robert Pratt <mtgmuppet@gmail.com>
2026-03-19 12:09:53 -0500

    refactor: split newspaper into per-view partials
    - Create news_index.html, news_options.html, news_data.html
    - Index uses page-header with settings action button
    - Go handler routes to correct template per sub-view

--- news.go
+++ news.go
@@ -1204,7 +1204,7 @@ func Newspaper(w http.ResponseWriter, r *http.Request) {
 		pageVars.Title = "This feature is not enabled"
 		pageVars.ErrorMessage = ErrMsgDenied
 
-		render(w, "news.html", pageVars)
+		render(w, "news_data.html", pageVars)
 		return
 	}
 
@@ -1228,7 +1228,7 @@ func Newspaper(w http.ResponseWriter, r *http.Request) {
 		pageVars.Title = "This feature is BANned"
 		pageVars.ErrorMessage = ErrMsgDenied
 
-		render(w, "news.html", pageVars)
+		render(w, "news_data.html", pageVars)
 		return
 	}
 
@@ -1284,7 +1284,7 @@ func Newspaper(w http.ResponseWriter, r *http.Request) {
 
 	switch page {
 	case "", "old":
-		render(w, "news.html", pageVars)
+		render(w, "news_index.html", pageVars)
 
 		return
 	case "options":
@@ -1293,7 +1293,7 @@ func Newspaper(w http.ResponseWriter, r *http.Request) {
 		pageVars.Editions = AllEditionsKeys
 		pageVars.EditionsMap = AllEditionsMap
 
-		render(w, "news.html", pageVars)
+		render(w, "news_options.html", pageVars)
 
 		return
 	case "syp":
@@ -1478,7 +1478,7 @@ func Newspaper(w http.ResponseWriter, r *http.Request) {
 			if newspage.Results == nil {
 				pageVars.InfoMessage = "This data is not ready yet, please try again in a few minutes"
 				pageVars.LastUpdate = time.Now()
-				render(w, "news.html", pageVars)
+				render(w, "news_data.html", pageVars)
 				return
 			}
 
@@ -1794,5 +1794,5 @@ func Newspaper(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	render(w, "news.html", pageVars)
+	render(w, "news_data.html", pageVars)
 }
