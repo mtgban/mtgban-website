0150611a53394839a9274d8faca783553c3d10f1
Vittorio Giovara <vittorio.giovara@gmail.com>
2026-03-26 12:57:58 +0100

    update-mtgban: Allow setting or removing a local snapshot

--- update-mtgban.sh
+++ update-mtgban.sh
@@ -5,7 +5,17 @@ TIP=$(git -c 'versionsort.suffix=-' ls-remote --exit-code --refs \
       --sort='version:refname' --tags  https://github.com/mtgban/go-mtgban.git '*.*.*' | \
       tail --lines=1 | cut -d "/" -f 3)
 
-if [[ "${1}" == "latest" ]]
+if [[ "${1}" == "local" ]]
+then
+    echo "Using local checkout at ../go-mtgban"
+    go mod edit -replace github.com/mtgban/go-mtgban=../go-mtgban
+    exit 0
+elif [[ "${1}" == "remote" ]]
+then
+    echo "Removing local replace directive"
+    go mod edit -dropreplace github.com/mtgban/go-mtgban
+    exit 0
+elif [[ "${1}" == "latest" ]]
 then
     echo "Using latest hash"
     TIP=$(git ls-remote https://github.com/mtgban/go-mtgban.git HEAD | awk '{ print $1}')
