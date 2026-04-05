b58a955ef791ba82e114d24ce046e4def695d2d1
Robert Pratt <mtgmuppet@gmail.com>
2026-04-05 11:58:03 -0500

    feat: add /api/chart/ JSON endpoint for mobile price history charts

--- main.go
+++ main.go
@@ -883,6 +883,7 @@ func main() {
 	http.Handle("/api/cardmarket/", enforceSigning(http.HandlerFunc(MKMHandler)))
 	http.Handle("/api/search/", enforceSigning(http.HandlerFunc(SearchAPI)))
 	http.Handle("/api/suggest", noSigning(http.HandlerFunc(SuggestAPI)))
+	http.Handle("/api/chart/", noSigning(http.HandlerFunc(ChartDataAPI)))
 	http.Handle("/api/opensearch.xml", noSigning(http.HandlerFunc(OpenSearchDesc)))
 	http.Handle("/api/load/datastore", noSigning(http.HandlerFunc(LoadDatastoreFromCloud)))
 	http.Handle("/api/load/", enforceAPISigning(http.HandlerFunc(LoadFromCloud)))
